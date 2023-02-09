2077984b23d4330eff7c2334bfdde0c57e93b6bf
emily <emily@bacheconsulting.com>
2023-02-09 15:32:39 +0100

    tests that cover all of Player

--- csharp/FantasyBattle.Tests/PlayerTest.cs
+++ csharp/FantasyBattle.Tests/PlayerTest.cs
@@ -5,48 +5,112 @@ using Xunit;
 
 namespace FantasyBattle.Tests
 {
-    public class PlayerTest
+    public class ItemTestData
     {
-        BasicItem leftHand = new BasicItem("round shield", 0, 1.4f);
-        BasicItem rightHand = new BasicItem("Excalibur", 20, 1.5f);
-        BasicItem head = new BasicItem("helmet of swiftness", 0, 1.2f);
-        BasicItem feet = new BasicItem("ten league boots", 0, 0.1f);
-        BasicItem chest = new BasicItem("breastplate of steel", 0, 1.4f);
+        public static BasicItem leftHand = new BasicItem("round shield", 0, 1.4f);
+        public static BasicItem rightHand = new BasicItem("Excalibur", 20, 1.5f);
+        public static BasicItem head = new BasicItem("helmet of swiftness", 0, 1.2f);
+        public static BasicItem feet = new BasicItem("ten league boots", 0, 0.1f);
+        public static BasicItem chest = new BasicItem("breastplate of steel", 0, 1.4f);
+    }
 
-        [Fact]
-        public void DamageCalculationsWithMocks()
+    public class PlayerTestWithStubs
+    {
+        Mock<IInventory> inventory = new Mock<IInventory>();
+        Mock<IEquipment> equipment = new Mock<IEquipment>();
+
+        public PlayerTestWithStubs()
         {
-            var inventory = new Mock<IInventory>();
-            var equipment = new Mock<IEquipment>();
             inventory.SetupGet(i => i.Equipment).Returns(equipment.Object);
-            equipment.SetupGet(e => e.LeftHand).Returns(leftHand);
-            equipment.SetupGet(e => e.RightHand).Returns(rightHand);
-            equipment.SetupGet(e => e.Head).Returns(head);
-            equipment.SetupGet(e => e.Feet).Returns(feet);
-            equipment.SetupGet(e => e.Chest).Returns(chest);
-            
+            equipment.SetupGet(e => e.LeftHand).Returns(ItemTestData.leftHand);
+            equipment.SetupGet(e => e.RightHand).Returns(ItemTestData.rightHand);
+            equipment.SetupGet(e => e.Head).Returns(ItemTestData.head);
+            equipment.SetupGet(e => e.Feet).Returns(ItemTestData.feet);
+            equipment.SetupGet(e => e.Chest).Returns(ItemTestData.chest);
+        }
+        
+        [Fact]
+        public void DamageCalculationsWithEmptyTarget()
+        {
             var stats = new Stats(1);
             var target = new Mock<Target>();
 
             var damage = new Player(inventory.Object, stats).CalculateDamage(target.Object);
+            
             Assert.Equal(114, damage.Amount);
         }
         
         [Fact]
-        public void DamageCalculationsWithFake() {
-            IInventory inventory = new FakeInventory(new FakeEquipment(leftHand, rightHand, head, feet, chest));
-            Stats stats = new Stats(1);
-            Target target = new FakeEnemy();
-            Damage damage = new Player(inventory, stats).CalculateDamage(target);
-            Assert.Equal(114, damage.Amount);
+        public void DamageCalculationsWithPlayerTarget()
+        {
+            var stats = new Stats(1);
+            var player = new Player(inventory.Object, stats);
+            var target = player;
+            
+            var damage = player.CalculateDamage(target);
+            
+            Assert.Equal(0, damage.Amount);
+        }        
+        [Fact]
+        public void DamageCalculationsWithSimpleEnemyTarget()
+        {
+            var stats = new Stats(1);
+            var player = new Player(inventory.Object, stats);
+            var target = new SimpleEnemy(new SimpleArmor(5), new List<Buff>{new BasicBuff(1.0f, 1.0f)});
+            
+            var damage = player.CalculateDamage(target);
+            
+            Assert.Equal(104, damage.Amount);
         }
     }
-    
-    
+
+    public class PlayerTestWithFakes
+    {
+        private FakeInventory inventory;
+        private FakeEquipment equipment;
+        
+        public PlayerTestWithFakes()
+        {
+            equipment = new FakeEquipment(ItemTestData.leftHand, ItemTestData.rightHand,
+                ItemTestData.head, ItemTestData.feet, ItemTestData.chest);
+            inventory = new FakeInventory(equipment);
+        }
+        [Fact]
+        public void DamageCalculationsWithEmptyTarget()
+        {
+            var stats = new Stats(1);
+            Target target = new FakeEnemy();
+            
+            var damage = new Player(inventory, stats).CalculateDamage(target);
+            
+            Assert.Equal(114, damage.Amount);
+        }
+        [Fact]
+        public void DamageCalculationsWithPlayerTarget()
+        {
+            var stats = new Stats(1);
+            var player = new Player(inventory, stats);
+            Target target = player;
+            
+            var damage = player.CalculateDamage(target);
+            
+            Assert.Equal(0, damage.Amount);
+        }        [Fact]
+        public void DamageCalculationsWithSimpleEnemyTarget()
+        {
+            var stats = new Stats(1);
+            var player = new Player(inventory, stats);
+            var target = new SimpleEnemy(new SimpleArmor(5), new List<Buff>{new BasicBuff(1.0f, 1.0f)});
+
+            var damage = player.CalculateDamage(target);
+            
+            Assert.Equal(104, damage.Amount);
+        }
+    }
+
 
     public class FakeEnemy : Target
     {
-        
     }
 
     public class FakeInventory : IInventory
@@ -85,11 +149,5 @@ namespace FantasyBattle.Tests
         public Item Head { get; }
         public Item Feet { get; }
         public Item Chest { get; }
-    }
-
-    public class FakeItem : Item
-    {
-        public int BaseDamage { get; }
-        public float DamageModifier { get; }
     }
 }
