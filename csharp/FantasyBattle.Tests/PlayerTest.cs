4586f9bf0c29eb89338aef8f1ad58d18a66f62f7
emily <emily@bacheconsulting.com>
2024-12-06 11:35:56 +0100

    Improve start position for C#

--- csharp/FantasyBattle.Tests/PlayerTest.cs
+++ csharp/FantasyBattle.Tests/PlayerTest.cs
@@ -1,31 +1,38 @@
 ﻿using System.Collections.Generic;
 using System.Text;
 using Moq;
+using NSubstitute;
 using Xunit;
 
 namespace FantasyBattle.Tests
 {
+    /*
+     * Helpful test data which corresponds to items listed in the top level README file
+     */
+    public class TestData
+    {
+        public static BasicItem shield = new BasicItem("round shield", 0, 1.4f);
+        public static BasicItem sword = new BasicItem("Flashy sword of danger", 10, 1.0f);
+        public static BasicItem excalibur = new BasicItem("Excalibur", 20, 1.5f);
+        public static BasicItem helmet = new BasicItem("helmet of swiftness", 0, 1.2f);
+        public static BasicItem boots = new BasicItem("ten league boots", 0, 0.1f);
+        public static BasicItem breastplate = new BasicItem("breastplate of steel", 0, 1.4f);
+        public static SimpleEnemy enemy = new SimpleEnemy(
+            new SimpleArmor(5), 
+            new List<Buff>() {new BasicBuff(1.0f, 1.0f)}
+            );
+    }
+    
     public class PlayerTest
     {
-
-        // choose this one if you are familiar with mocks
-        [Fact(Skip = "Test is not finished yet")]
-        public void DamageCalculationsWithMocks() {
-            var inventory = new Mock<Inventory>();
-            var stats = new Stats(1);
-            var target = new Mock<Target>();
-
-            var damage = new Player(inventory.Object, stats).CalculateDamage(target.Object);
-            Assert.Equal(9, damage.Amount);
-        }
-
-        // choose this one if you are not familiar with mocks
         [Fact(Skip = "Test is not finished yet")]
         public void DamageCalculations() {
             Inventory inventory = new Inventory(null);
             Stats stats = new Stats(1);
-            Target target = new SimpleEnemy(null, null);
-            Damage damage = new Player(inventory, stats).CalculateDamage(target);
+            var player = new Player(inventory, stats);
+
+            Damage damage = player.CalculateDamage(null);
+            
             Assert.Equal(9, damage.Amount);
         }
     }
